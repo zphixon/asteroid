a08f6d257e173a6173d59787d5c2675fbd18a159
Zack <zackh@firemail.cc>
2017-10-04 22:29:20 -0400

    update circle, add ellipse
    using a different implementation of bresenham's algorithm which removes extra
    pixels

--- src/graphics.rs
+++ src/graphics.rs
@@ -17,10 +17,10 @@ impl Graphics {
     pub fn draw(&mut self, shape: Draw) {
         match shape {
             Draw::CircleFill {x, y, r} => {
-                fill_circle(&mut self.canvas, x + r, y + r, r);
+                draw_circle(&mut self.canvas, true, x + r, y + r, r);
             },
             Draw::CircleLine {x, y, r} => {
-                draw_circle(&mut self.canvas, x + r, y + r, r);
+                draw_circle(&mut self.canvas, false, x + r, y + r, r);
             },
             Draw::RectangleFill {x, y, w, h} => {
                 self.canvas.fill_rect(::sdl2::rect::Rect::new(x, y, w, h)).unwrap();
@@ -28,9 +28,12 @@ impl Graphics {
             Draw::RectangleLine {x, y, w, h} => {
                 self.canvas.draw_rect(::sdl2::rect::Rect::new(x, y, w, h)).unwrap();
             },
-            Draw::Ellipse {x, y, f} => {},
-            Draw::Triangle {x, y, b, h} => {},
-            _ => {}
+            Draw::EllipseFill {x, y, w, h} => {
+                draw_ellipse(&mut self.canvas, true, x, y, w, h);
+            },
+            Draw::EllipseLine {x, y, w, h} => {
+                draw_ellipse(&mut self.canvas, false, x, y, w, h);
+            },
             //Draw::Triangle {x, y, b, h} => {},
             s => {
                 unimplemented!("not implemented: {:?}", s);
@@ -67,10 +70,17 @@ pub enum Draw {
         w: u32,
         h: u32,
     },
-    Ellipse {
+    EllipseFill {
         x: i32,
         y: i32,
-        f: i32,
+        w: i32,
+        h: i32,
+    },
+    EllipseLine {
+        x: i32,
+        y: i32,
+        w: i32,
+        h: i32,
     },
     Triangle {
         x: i32,
@@ -80,57 +90,98 @@ pub enum Draw {
     }
 }
 
-fn draw_circle(canvas: &mut Canvas<Window>, cx: i32, cy: i32, r: i32) {
-    let mut x = r - 1;
-    let mut y = 0;
-    let mut dx = 1;
-    let mut dy = 1;
-    let mut err = dx - (r << 1);
+fn draw_circle(canvas: &mut Canvas<Window>, fill: bool, cx: i32, cy: i32, r: i32) {
+    let mut x = 0;
+    let mut y = r;
+    let mut d = 2 - 2 * r;
+    let mut err;
 
-    while x >= y {
-        canvas.draw_point(Point::new(cx + x, cy + y)).unwrap();
-        canvas.draw_point(Point::new(cx + y, cy + x)).unwrap();
-        canvas.draw_point(Point::new(cx - y, cy + x)).unwrap();
-        canvas.draw_point(Point::new(cx - x, cy + y)).unwrap();
-        canvas.draw_point(Point::new(cx - x, cy - y)).unwrap();
-        canvas.draw_point(Point::new(cx - y, cy - x)).unwrap();
-        canvas.draw_point(Point::new(cx + y, cy - x)).unwrap();
-        canvas.draw_point(Point::new(cx + x, cy - y)).unwrap();
-
-        if err <= 0 {
-            y += 1;
-            err += dy;
-            dy += 2;
+    while y >= 0 {
+        if fill {
+            canvas.draw_line(Point::new(cx + x, cy + y), Point::new(cx - x, cy + y)).unwrap();
+            canvas.draw_line(Point::new(cx + x, cy - y), Point::new(cx - x, cy - y)).unwrap();
         } else {
-            x -= 1;
-            dx += 2;
-            err += (-r << 1) + dx;
+            canvas.draw_point(Point::new(cx + x, cy + y)).unwrap();
+            canvas.draw_point(Point::new(cx - x, cy + y)).unwrap();
+            canvas.draw_point(Point::new(cx + x, cy - y)).unwrap();
+            canvas.draw_point(Point::new(cx - x, cy - y)).unwrap();
         }
+
+        err = 2 * (d + y) - 1;
+        if d < 0 && err <= 0 {
+            x += 1;
+            d += 2 * x + 1;
+            continue;
+        }
+
+        err = 2 * (d - x) - 1;
+        if d > 0 && err > 0 {
+            y -= 1;
+            d += 1 - 2 * y;
+            continue;
+        }
+
+        x += 1;
+        d += 2 * (x - y);
+        y -= 1;
     }
 }
 
-fn fill_circle(canvas: &mut Canvas<Window>, cx: i32, cy: i32, r: i32) {
-    let mut x = r - 1;
-    let mut y = 0;
-    let mut dx = 1;
-    let mut dy = 1;
-    let mut err = dx - (r << 1);
+fn draw_ellipse(canvas: &mut Canvas<Window>, fill: bool, ex: i32, ey: i32, w: i32, h: i32) {
+    let ex = ex + w;
+    let ey = ey + h;
 
-    while x >= y {
-        canvas.draw_line(Point::new(cx + x, cy + y), Point::new(cx - x, cy + y)).unwrap();
-        canvas.draw_line(Point::new(cx + y, cy + x), Point::new(cx - y, cy + x)).unwrap();
-        canvas.draw_line(Point::new(cx - x, cy - y), Point::new(cx + x, cy - y)).unwrap();
-        canvas.draw_line(Point::new(cx - y, cy - x), Point::new(cx + y, cy - x)).unwrap();
+    let a2 = w * w;
+    let b2 = h * h;
+    let fa2 = 4 * a2;
+    let fb2 = 4 * b2;
 
-        if err <= 0 {
-            y += 1;
-            err += dy;
-            dy += 2;
+    let mut x = 0;
+    let mut y = h;
+    let mut sigma = 2 * b2 + a2 * (1 - 2 * h);
+
+    while b2 * x <= a2 * y {
+        if fill {
+            canvas.draw_line(Point::new(ex + x, ey + y), Point::new(ex - x, ey + y)).unwrap();
+            canvas.draw_line(Point::new(ex + x, ey - y), Point::new(ex - x, ey - y)).unwrap();
         } else {
-            x -= 1;
-            dx += 2;
-            err += (-r << 1) + dx;
+            canvas.draw_point(Point::new(ex + x, ey + y)).unwrap();
+            canvas.draw_point(Point::new(ex - x, ey + y)).unwrap();
+            canvas.draw_point(Point::new(ex + x, ey - y)).unwrap();
+            canvas.draw_point(Point::new(ex - x, ey - y)).unwrap();
         }
+
+        if sigma >= 0 {
+            sigma += fa2 * (1 - y);
+            y -= 1;
+        }
+
+        sigma += b2 * ((4 * x) + 6);
+        x += 1;
+    }
+
+    let mut x = w;
+    let mut y = 0;
+    let mut sigma = 2 * a2 + b2 * (1 - 2 * w);
+
+    while a2 * y <= b2 * x {
+        if fill {
+            canvas.draw_line(Point::new(ex + x, ey + y), Point::new(ex - x, ey + y)).unwrap();
+            canvas.draw_line(Point::new(ex + x, ey - y), Point::new(ex - x, ey - y)).unwrap();
+        } else {
+            canvas.draw_point(Point::new(ex + x, ey + y)).unwrap();
+            canvas.draw_point(Point::new(ex - x, ey + y)).unwrap();
+            canvas.draw_point(Point::new(ex + x, ey - y)).unwrap();
+            canvas.draw_point(Point::new(ex - x, ey - y)).unwrap();
+        }
+
+        if sigma >= 0 {
+            sigma += fb2 * (1 - x);
+            x -= 1;
+        }
+
+        sigma += a2 * ((4 * y) + 6);
+        y += 1;
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -40,8 +40,10 @@ impl GameState for Game {
     fn render(&self, args: &mut Args, graphics: &mut Graphics) -> AsteroidResult {
         graphics.set_color(255, 255, 255);
         graphics.draw(Draw::CircleFill {x: self.cx as i32, y: self.cy as i32, r: self.cr as i32});
+        graphics.draw(Draw::EllipseLine {x: self.cx as i32, y: self.cy as i32, w: self.cr as i32 * 2, h: self.cr as i32});
         graphics.set_color(0, 50, 255);
         graphics.draw(Draw::RectangleLine {x: (self.cx - 30.) as i32, y: (99. - self.cy * 3.) as i32, w: self.cr as u32, h: (2. * self.cr / 3.) as u32});
+        //graphics.draw(Draw::Triangle {x: 0, y: 0, b: 0, h: 0});
         AsteroidResult::Ok
     }
 }
